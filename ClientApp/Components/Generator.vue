d1d2f9f02b486381971629c1d8946d567eb8cae5
nikitades <skytremolo@gmail.com>
2019-01-21 00:51:48 +0300

    Fixed levels hell

--- ClientApp/Components/Generator.vue
+++ ClientApp/Components/Generator.vue
@@ -35,7 +35,7 @@ export default {
 <template>
   <div class="m-t-10">
     <div class="columns">
-      <div class="column is-one-third-tablet">
+      <div class="column">
         <p class="help">Количество вещей</p>
         <div class="field">
           <div class="control has-icons-left">
@@ -58,29 +58,22 @@ export default {
           </div>
         </div>
       </div>
-      <div class="column one-quarter-tablet">
-        <div class="buttons m-l-10 m-t-20">
+      <div class="column">
+        <p class="help">&nbsp;</p>
+        <div class="buttons">
           <a href="#" @click.prevent="decrement" class="button is-link has-text-weight-bold">-</a>
           <a href="#" @click.prevent="increment" class="button is-link has-text-weight-bold">+</a>
         </div>
       </div>
       <div class="column">
-        <div class="level">
-          <div class="level-left"></div>
-          <div class="level-right">
-            <div class="m-t-20">
-                <template v-if="mode === 'main'">
-                  <a href="#" @click.prevent="toggleMode" class="button is-success">
-                    Заглянуть в сундучок
-                    </a>
-                  </template>
-                <template v-if="mode === 'stash'">
-                  <a href="#" @click.prevent="toggleMode" class="button is-warning">
-                    Прочь из сундучка
-                    </a>
-                </template>
-            </div>
-          </div>
+        <p class="help">&nbsp;</p>
+        <div>
+          <template v-if="mode === 'main'">
+            <a href="#" @click.prevent="toggleMode" class="button is-success is-pulled-right">Заглянуть в сундучок</a>
+          </template>
+          <template v-if="mode === 'stash'">
+            <a href="#" @click.prevent="toggleMode" class="button is-warning is-pulled-right">Прочь из сундучка</a>
+          </template>
         </div>
       </div>
     </div>
